62ea766d18a9e5189722c6c76dfcb22358135714
petejh <petejh.code@q.com>
2020-03-23 15:36:45 -0700

    Add unit tests for `layer_forward()`

--- test/test_deepen_model.py
+++ test/test_deepen_model.py
@@ -1,4 +1,6 @@
 import unittest
+from unittest.mock import patch
+
 import numpy as np
 
 from deepen import model
@@ -54,25 +56,84 @@ class DeepenModelLinearForwardTest(unittest.TestCase):
         self.A = np.array([[1], [2]])
         self.W = np.array([[1, 2], [3, 4], [5, 6]])
         self.b = np.array([[1], [2], [3]])
+
+        self.params = (self.A, self.W, self.b)
+
+        # Z = W·A + b
         self.Z_expected = np.array([[6], [13], [20]])
 
     def test_Z_has_the_correct_shape(self):
-        Z, _ = model.linear_forward(self.A, self.W, self.b)
+        Z, _ = model.linear_forward(*self.params)
 
         self.assertEqual(Z.shape, self.Z_expected.shape)
 
     def test_Z_is_linear_combination_of_the_inputs(self):
-        Z, _ = model.linear_forward(self.A, self.W, self.b)
+        Z, _ = model.linear_forward(*self.params)
 
         self.assertTrue(np.array_equal(Z, self.Z_expected))
 
     def test_cache_contains_the_inputs(self):
-        _, cache = model.linear_forward(self.A, self.W, self.b)
+        _, cache = model.linear_forward(*self.params)
 
-        subtests = zip(cache, (self.A, self.W, self.b), ('A', 'W', 'b'))
+        subtests = zip(cache, self.params, ('A', 'W', 'b'))
         for cached, param, description in subtests:
             with self.subTest(parameter=description):
                 self.assertTrue(np.array_equal(cached, param))
+
+class DeepenModelLayerForwardTest(unittest.TestCase):
+    def setUp(self):
+        self.A = np.array([[1], [2]])
+        self.W = np.array([[1, 2], [3, 4], [5, 6]])
+        self.b = np.array([[1], [2], [3]])
+
+        self.params = (self.A, self.W, self.b)
+
+        # Z = W·A + b
+        self.Z = np.array([[6], [13], [20]])
+        # relu(Z)
+        self.relu = np.array([[6], [13], [20]])
+        # sigmoid(Z)
+        self.sigmoid = np.array([[0.99752738], [0.99999774], [1.0]])
+
+    def test_A_has_the_correct_shape(self):
+        A, _ = model.layer_forward(*self.params, 'relu')
+
+        self.assertTrue(A.shape == self.Z.shape)
+
+    def test_linear_cache_contains_the_inputs(self):
+        _, (linear_cache, _) = model.layer_forward(*self.params, 'relu')
+
+        subtests = zip(linear_cache, self.params, ('A', 'W', 'b'))
+        for cached, param, description in subtests:
+            with self.subTest(parameter=description):
+                self.assertTrue(np.array_equal(cached, param))
+
+    def test_activation_cache_has_the_correct_shape(self):
+        _, (_, activation_cache) = model.layer_forward(*self.params, 'relu')
+
+        self.assertTrue(activation_cache.shape == self.Z.shape)
+
+    def test_calls_relu_activation(self):
+        relu_returns = (self.relu, self.Z)
+
+        with unittest.mock.patch(
+            'deepen.model.relu',
+            return_value = relu_returns
+        ) as relu:
+            model.layer_forward(*self.params, 'relu')
+
+            relu.assert_called_once()
+
+    def test_calls_sigmoid(self):
+        sigmoid_returns = (self.sigmoid, self.Z)
+
+        with unittest.mock.patch(
+            'deepen.model.sigmoid',
+            return_value = sigmoid_returns
+        ) as sigmoid:
+            model.layer_forward(*self.params, 'sigmoid')
+
+            sigmoid.assert_called_once()
 
 if __name__ == '__main__':
     unittest.main()
